5c1a14d60b3adeaae31073c88373f0c5aa56b9e3
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-06-08 16:37:47 +0530

    Introduction to C++

---
+++ Intduction.cpp
@@ -0,0 +1,17 @@
+//C++ is a popular Language now a day . Its mainly used for game developement and for computer programs
+//C++ is an extension version of c its having almost same syntax like c , but c++ is object oriented programming language
+//whereas c is procedural language.
+// C++ was developed by Bjarne Stroustrup in 1979.
+
+//Sample program.
+#include<iostream>
+//In C++, the # symbol is used to indicate preprocessor directives.
+// Preprocessor directives are instructions that are executed by the preprocessor before the actual compilation of the code begins.
+//iostream has inbulilt function of cout cin and many more
+
+using namespace std;
+int main() {
+    cout<<"Hello World";
+
+    return 0;
+}
