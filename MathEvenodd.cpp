afdfc234b12184056197cd48e9613f08f6840bc0
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-06-28 20:23:12 +0530

    Math Even Odd program
    if(num % 2 == 0) { cout<<"even"; } else { cout<<"odd"; }

---
+++ MathEvenodd.cpp
@@ -0,0 +1,17 @@
+// Write a program to check weather the number is even or odd
+#include<iostream>
+using namespace std;
+int main() {
+int num;
+cout<<"Enter the Number :";
+cin>>num;
+cout<<"\n";
+if(num%2==0){
+    cout<<num << " is even number";
+}
+else{
+    cout<<num << " is even number";
+}
+return 0;
+}
+
