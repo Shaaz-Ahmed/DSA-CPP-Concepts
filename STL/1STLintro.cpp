8051e25c9e44197b87d22811d1cf96546bdd047f
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-06-29 08:08:07 +0530

    Introduction to STL
    STL is standard library template.
    STL has 4 different components: 1 Container 2 Iterators 3 Functions 4 Algorithm

---
+++ STL/1STLintro.cpp
@@ -0,0 +1,23 @@
+
+// 87 6:13
+// STL : stands for standard template library.
+// STL is basically compilation of algorithms , containers , iterators , functions with minimized version
+// STL is basically the template that provided general purpose classes and functions
+// components of STL:
+// There are four component of STL
+// 1. container
+// 2. iterator
+// 3. algorithm
+// 4. functions
+// for all the time we have to include all the library suppose we are using math function then we have to import math library if we are using string function at that we have to import string library
+// for avoiding all of this we have to simple use the #include<bits/stdc++.> this normally having all types of library in it.
+
+// first we will look at the containers then iterators at end we will look function and different different algorithm
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+ cout<<"Hello world";
+
+ return 0;
+}
+
