42d295165203fff6972b12033811e98c73b98bac
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-07-04 09:10:10 +0530

    Area of square
    area= s*s
    cout<<area;

---
+++ areaofsquare.cpp
@@ -0,0 +1,13 @@
+// WAP for find the area of square
+#include<iostream>
+using namespace std;
+int main() {
+    int s, area;
+
+        cout<<"Enter the area of square : ";
+        cin>>s;
+
+        area = s*s;
+        cout<<"area of square is : "<<area;
+return 0;
+}
