569bece53ca4371785fb0c7a0605e346b45cdcfb
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-07-04 09:06:52 +0530

    Area of circle
    3.14*r*r

---
+++ math/areaofcircle.cpp
@@ -0,0 +1,12 @@
+// Area of circle : 3.14*r*r;
+#include<iostream>
+using namespace std;
+int main() {
+    int r,area;
+    cout<<"Enter the radius of circle : ";
+    cin>>r;
+
+    area  = 3.14*r*r;
+    cout<<"Area of circle is : "<<area;
+return 0;
+}
