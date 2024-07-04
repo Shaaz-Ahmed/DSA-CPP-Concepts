bdcd5cbdf5d39ae93dd237d065b3edb0e860b289
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-07-04 08:30:28 +0530

    Print first N natural number
    int n; for(int i=0 ; i<n; i++)  { cout<<i<<ends;

---
+++ math/FirstNaturalnp.cpp
@@ -0,0 +1,12 @@
+// WAP for first N natural Numbers
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+    for(int i=0; i<n ; i++){
+        cout<<i<<ends;
+    }
+return 0;
+}
