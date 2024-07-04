5318cec7cc34e8e7f9eb4ac19239ebcd9c834228
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-07-04 10:03:49 +0530

    Area of Rectangle using c++
    area = length * breadth

---
+++ math/areaofRectan.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int l , b , area;
+
+    cout<<"Enter the length and breadth of rectangle :";
+    cin>>l>>b;
+
+    area = l*b;
+    cout<<"Length and breadth of rectangle is : "<<area;
+return 0;
+}
