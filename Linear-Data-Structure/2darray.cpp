fe8e0f8662c890b8172ff0b74fb24b2e555da135
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-07-07 15:20:48 +0530

    Add files via upload

---
+++ Linear-Data-Structure/2darray.cpp
@@ -0,0 +1,11 @@
+// 2D Array : two dimensional array
+// 2d array has two parameters
+// n : Numbers of rows
+// m : Numbers of coloumns
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Hello world";
+return 0;
+}
