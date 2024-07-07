8757fd0c0e91aa13052623fcb6ad015263466ba0
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-07-07 15:31:19 +0530

    2D array
    A 2d array is like a matrix, we have to take the input and we have to print it
    for 2d we required 2 loop i & j 
    first we have to take input then we have to print output

--- Linear-Data-Structure/2darray.cpp
+++ Linear-Data-Structure/2darray.cpp
@@ -6,6 +6,21 @@
 #include<iostream>
 using namespace std;
 int main() {
-    cout<<"Hello world";
+    int n , m;
+    cin>>n>>m;
+
+    int arr[n][m];
+        for(int i=0; i<n ;i++){
+            for (int j=0; j<m; j++){
+                cin>>arr[i][j];
+            }
+        }
+        cout<<"Matrix is : \n";
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                cout<<arr[i][j]<< " ";
+            }
+            cout<<"\n";
+        }
 return 0;
 }
