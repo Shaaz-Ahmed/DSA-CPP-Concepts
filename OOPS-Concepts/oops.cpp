434da32d3a901383ccf3e6d9a8b4603e46a619aa
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-07-07 15:57:52 +0530

    Introduction to oop
    This file only having sample info about OOPS

---
+++ OOPS-Concepts/oops.cpp
@@ -0,0 +1,23 @@
+// OOP : stands for Object oriented programming.
+// Procedural programming language is about writing the procedure and function and perform a task but in oops it having both object and function
+// OOP is much faster than procedural programming.
+// Class and object are two main aspect of object-oriented programming language.
+// class is a template for object and object is a instance of a class.
+// When the individual object is created then it inheriting all the function from class.
+
+
+// example of class and object.
+// class is fruits.
+// object apple , banana , mango.
+
+// In c++ everything is object lets understand with real life example
+// A car is a object and it has different attribute & methods.
+// Attribute is like variable of that object. (weight and color)
+// Methods are like function of that object. like what functionality it can do ( drive and brake )
+
+#include<iostream>
+using namespace std;
+int main() {
+
+}
+
