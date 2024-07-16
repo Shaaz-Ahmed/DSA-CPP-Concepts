0efa054881fbd5f11eb1c7f4e7c8818ead927b69
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-07-16 07:41:46 +0530

    Hashing Methods - Division Methods
    Here we use module , we basically do (%) of arr[i] and we store that element at we got the remainder

---
+++ Hashing/HashingMethods.cpp
@@ -0,0 +1,12 @@
+// There are three types of hashing method
+// 1. Division Methods
+// 2. Fold methods
+// 3. Mid square methods
+
+// *****DIvision Methods ******
+// an array of greater than 10 is not allowed
+// we module the a[i]
+
+
+// we have to do simple module for every element in the array  that is arr[i] % 10
+// If the remainder is same then make it chain in sorted order. (follow from book for better understanding)
