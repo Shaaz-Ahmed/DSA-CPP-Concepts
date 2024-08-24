766eacdc9de14ed73e2575e76ceab735cfcd499e
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-08-24 23:42:39 +0530

    Add files via upload

--- Dummy/3sum.cpp
+++ Dummy/3sum.cpp
@@ -1,32 +1,27 @@
-#include <bits/stdc++.h>
-vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
-	vector<vector<int>> ans;
-	sort(arr.begin(), arr.end());
-	for (int i=0;i<arr.size() - 2;i++) {
-		if (i > 0 && arr[i] == arr[i-1]) {
-			continue;
-		}
-		int left = (i + 1), right = (arr.size() - 1);
-		while (left < right) {
-			int sum = (arr[i] + arr[left] + arr[right]);
-			if (sum == K) {
-				ans.push_back({arr[i], arr[left], arr[right]});
-				int x = left;
-				while (left < right && arr[left] == arr[x]) {
-					left++;
-				}
-				x = right;
-				while (left < right && arr[right] == arr[x]) {
-					right--;
-				}
-			}
-			else if (sum < K) {
-				left++;
-			}
-			else {
-				right--;
-			}
-		}
-	}
-	return ans;
+vector<vector<int>> triplet(int n, vector<int> &arr)
+{
+    // Write your code here.
+    vector<vector<int>>ans;
+    sort(arr.begin(), arr.end() );
+    for(int i =0; i<n; i++){
+        if (i != 0 && arr[i] == arr[i - 1]) continue;
+        int start =i+1,end =n-1;
+        while(start<end){
+            int target = -arr[i];
+            int sum = arr[start] +arr[end];
+            if(sum == target ){
+                ans.push_back( {arr[i], arr[start], arr[end]} );
+                start++,end--;
+                while(start<end && arr[start] == arr[start-1]) start++;
+                while(start<end && arr[end] == arr[end+1]) end--;
+            }else if (sum<target){
+                start++;
+            }else{
+                end--;
+            }
+        }
+    }
+    return ans;
+
 }
+// follow theory from book
