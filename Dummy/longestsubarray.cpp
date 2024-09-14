d548ddd15b8860849b5064b91bdbe22d434f0373
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-09-14 23:50:19 +0530

    Delete Dummy/longestsubarray.cpp

--- Dummy/longestsubarray.cpp
+++
@@ -1,8 +0,0 @@
-// longest sub array with sum k
-
-// longest sub array means contigious part of array
-
-step 1 : generate all the sub array from index 1 and 2 index 1 and 3 index 1 4 and so on once i complete index 1 ill shift to index 2
-index 2 and 3 2 and 4 2 and 5 and so on till the end
-
-i and j but j will start always from i
