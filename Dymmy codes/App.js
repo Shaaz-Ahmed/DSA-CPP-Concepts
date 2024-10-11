34e787c27aa98c050efa4e548866acd1995e609b
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-10-11 22:59:02 +0530

    Add files via upload

---
+++ Dymmy codes/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { AppContext } from './context/contextApi';
+
+const App = ()=>{
+  return <AppContext> <div className="text-3xl">App</div> </AppContext>
+}
+export default App;
