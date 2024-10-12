e80df63488de54e48051b44631efc605c0d6ed4a
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-10-12 22:27:23 +0530

    Add files via upload

---
+++ Dymmy codes/191Noof1bit.cpp
@@ -0,0 +1,30 @@
+//   Problem statement: 191. Numbers of 1 Bits
+Write a function that takes the binary representation of a positive integer and returns the number of
+set bits
+ it has (also known as the Hamming weight).
+
+
+
+Example 1:
+
+Input: n = 11
+
+Output: 3
+
+Explanation:
+
+The input binary string 1011 has a total of three set bits
+
+solution:
+    // we have to use and operator and right shift operator
+    class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while (n > 0) {
+            count += (n & 1);
+            n >>= 1;
+        }
+        return count;
+    }
+};
