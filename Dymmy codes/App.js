f2938c2be4e18076fb315322fff90f07037de9fa
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-10-23 23:39:22 +0530

    Delete Dymmy codes/App.js

--- Dymmy codes/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { AppContext } from './context/contextApi';
-
-const App = ()=>{
-  return <AppContext> <div className="text-3xl">App</div> </AppContext>
-}
-export default App;
