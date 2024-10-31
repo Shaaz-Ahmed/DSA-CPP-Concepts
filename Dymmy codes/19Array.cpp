b0f1511d65f7520baad721da878e63462c39ae00
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-10-31 21:50:39 +0530

    Add files via upload

---
+++ Dymmy codes/19Array.cpp
@@ -0,0 +1,24 @@
+/*
+ Array is a collection of a similar data types
+ syntax:
+   data_types array_name array_size = {1,2,3,4}
+   int arr[4] = {10,20,30,40}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int arr[6] = {10,20,30,40,50,60};  //initialize
+  cout<<arr[3];
+  return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int n,i;
+  cout<<"Enter the element in array";
+  cin>>n;
+     for(i=0;i<n;i++)
+        return 0 ;
+}
