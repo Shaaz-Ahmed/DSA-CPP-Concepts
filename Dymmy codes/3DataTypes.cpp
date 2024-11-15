29e3fd5b774084cdfdfbdda4d5b176433e839c57
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-11-15 17:18:56 +0530

    Add files via upload

---
+++ Dymmy codes/3DataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //There are 4 types of data types 1. Basic DataTypes/Non-Primitive (int, char, float,double,etc) 2.Derived Dataype/ Primtive (Array, pointer,etc)
+  //3.Enumeration Data Type (enum) 4.User Define Data types (structure)
+
+  //Primitive Datatypes
+  int x = 10 ;
+  float y = 20 ;
+  char z = 's' ;
+
+   cout<<x <<"\n";
+   cout<<y <<"\n";
+   cout<<z;
+
+return 0;
+}
